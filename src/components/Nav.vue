aafcf8b8fdc70e6ce9af7647f9da3d442f90a230
Debbie <1770554944@qq.com>
2021-07-12 15:09:39 +0800

    创建Nav组件，404页面“ â

---
+++ src/components/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <router-link to="/Statistics">流水</router-link>
+  <router-link to="/money">记一笔</router-link>
+  <router-link to="/labels">标签</router-link>
+</div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  当前页面不存在，请检查网址是否正确
+  <br>
+  <router-link to="/money">返回首页</router-link>
+</div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
