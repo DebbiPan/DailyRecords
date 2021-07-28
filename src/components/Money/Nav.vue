ac64407b26e3e09e4299e949ff497ca4e09ac76e
Debbie <1770554944@qq.com>
2021-07-28 14:32:24 +0800

    模块化Nav

---
+++ src/components/Money/Nav.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav>
+    <Icon name="back" class="back"></Icon>
+    <ul class="types">
+      <li class="expenditure selected">支出</li>
+      <li class="income">收入</li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import Icon from "@/components/Icon.vue";
+export default{
+  name:'Nav',
+  components:{Icon}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+nav{
+  width:100%;
+  height: 50px;
+  line-height: 50px;
+  background: $pink;
+  color:$white;
+  >.back{
+    float: left;
+    width:30px;
+    height:30px;
+    position: relative;
+    top:10px;
+  }
+  >ul{
+    display: flex;
+    flex-direction: row;
+    justify-content:space-around ;
+    font-size: $font-nav;
+    >li {
+      position:relative;
+      &.selected::after{
+        content:'';
+        width:60px;
+        height:3px;
+        background: $white;
+        position:absolute;
+        bottom:0;
+        left:-5px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="wrap">
-    <nav>
-      <Icon name="back" class="back"></Icon>
-      <ul class="types">
-        <li class="expenditure selected">支出</li>
-        <li class="income">收入</li>
-      </ul>
-    </nav>
+    <Nav/>
     <div class="tags">
       <ul>
         <li>
@@ -166,48 +160,17 @@
 
 <script lang="ts">
 import Icon from "@/components/Icon.vue";
+import Nav from "@/components/Money/Nav.vue";
 
 export default {
   name:'money',
-  components:{Icon}
+  components:{Icon,Nav}
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
 .wrap{
-  >nav{
-    width:100%;
-    height: 50px;
-    line-height: 50px;
-    background: $pink;
-    color:$white;
-    >.back{
-      float: left;
-      width:30px;
-      height:30px;
-      position: relative;
-      top:10px;
-    }
-    >ul{
-      display: flex;
-      flex-direction: row;
-      justify-content:space-around ;
-      font-size: $font-nav;
-      >li {
-        position:relative;
-        &.selected::after{
-          content:'';
-          width:60px;
-          height:3px;
-          background: $white;
-          position:absolute;
-          bottom:0;
-          left:-5px;
-        }
-      }
-    }
-  }
   >.tags{
     overflow: auto;
     margin-bottom: 20px;
