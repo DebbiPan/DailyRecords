ca4d45035298e33fc020735d9026daf795047308
Debbie <1770554944@qq.com>
2021-08-24 15:22:17 +0800

    完成数据整理页面

--- src/components/statistics/RecordList.vue
+++ src/components/statistics/RecordList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <div class="day" v-for="(group,index) in groupedList" :key="index">
+    <div class="day" v-for="group in groupedList" :key="group.title">
       <div class="dayTitle">
         <div class="dateRecord">
           <span class="date">{{ group.title }}</span>
@@ -9,20 +9,20 @@
         <div class="total">
           <div class="out">
             <span class="text">收入：</span>
-            <span class="number">0.00</span>
+            <span class="number">{{group.total.in}}</span>
           </div>
           <div class="in">
             <span class="text">支出：</span>
-            <span class="number">0.00</span>
+            <span class="number">{{group.total.out}}</span>
           </div>
         </div>
       </div>
       <div class="count" v-for="(item,index) in group.items" :key="index">
         <Icon name="car" class="icon"/>
-        <div class="text">{{item.tags}}</div>
-        <div class="notes">{{item.notes}}</div>
-        <div class="amount" v-if="item.type === '-'">-{{item.amount}}</div>
-        <div class="amount" v-if="item.type === '+'">{{item.amount}}</div>
+        <div class="text">{{ item.tags.tag }}</div>
+        <div class="notes">{{ item.notes }}</div>
+        <div class="amount" v-if="item.type === '-'">-{{ item.amount }}</div>
+        <div class="amount" v-if="item.type === '+'">{{ item.amount }}</div>
       </div>
     </div>
     <div class="block"></div>
@@ -46,33 +46,44 @@ export default class RecordList extends Vue {
     // eslint-disable-next-line no-undef
     return (this.$store.state as RootState).recordList;
   }
-  // tagString(tags: Tag[]) {
-  //   return tags.length === 0 ? '无' : tags.map(t => t.name).join(',');
-  // }
 
   get groupedList() {
     const {recordList} = this;
 
     const newList = clone(recordList)
-        .sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());
+        .sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());//排序，将数组按照时间从小到大排序
+
+    type Total = { all: number, in: number, out: number };
+    type Result = { title: string, total?: Total, items: RecordList[] }[]
 
     if (newList.length === 0) {return [];}
-    type Result = { title: string, total?: number, items: RecordList[] }[] //使用数组变成有序的
-    const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+    const result: Result = [{title: dayjs(newList[0].createAt).format('YYYY-MM-DD'), items: [newList[0]]}];
     for (let i = 1; i < newList.length; i++) {
       const current = newList[i];
       const last = result[result.length - 1];
       if (dayjs(last.title).isSame(dayjs(current.createAt), 'day')) {
         last.items.push(current);
       } else {
-        result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
+        result.push({title: dayjs(current.createAt).format('YYYY-MM-DD'), items: [current]});
+
       }
     }
-    result.map(group => {
-      group.total = group.items.reduce((sum, item) => {
-        return sum + item.amount;
-      }, 0);
-    });
+    for (let i = 0; i < result.length; i++) {
+      let inAmount = 0;
+      let outAmount = 0;
+      let allAmount = 0;
+      const current = result[i].items;
+      if (current) {
+        current.filter(i => i.type === '+').map(i =>
+            inAmount += i.amount);
+        current.filter(i => i.type === '-').map(i =>
+            outAmount -= i.amount);
+      } else {
+        return [];
+      }
+      allAmount = inAmount + outAmount;
+      result[i].total = {all: allAmount, in: inAmount, out: outAmount};
+    }
     return result;
   }
 }
@@ -120,14 +131,16 @@ export default class RecordList extends Vue {
         height: 24px;
         margin: 5px;
       }
-      .notes{
+
+      .notes {
         font-size: 14px;
-        margin-left:10px;
-        color:$darkgrey;
+        margin-left: 10px;
+        color: $darkgrey;
         overflow: hidden;
         white-space: nowrap;
-        text-overflow:ellipsis;
+        text-overflow: ellipsis;
       }
+
       .amount {
         flex-grow: 1;
         text-align: right;
